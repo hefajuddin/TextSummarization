72ff88f345c77a1512a0b8544289ca13cb2308af
Hefaj Uddin <hefajuddin101@gmail.com>
2024-11-30 23:08:29 +0600

    fix data loader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,6 +1,7 @@
 import json
-file_path="data/sample_texts.json"
+
 def load_data(file_path):
+    """Load sample texts from a JSON file."""
     with open(file_path, 'r') as f:
         data = json.load(f)
     return data
