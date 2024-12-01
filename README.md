b6e04880d7eddbfa4c68c8485d9f81ce2ab12365
Hefaj Uddin <hefaj@ecosoftbd.com>
2024-12-01 18:18:03 +0600

    update README, sample_texts and train files

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 pip install -r requirements.txt
 
 2. run project executing the following command-
-python train.py
-It may take some time depending on your cpu/gpu performane
+    i. python train.py
+    ii. prompt expect a detail text to summarize, just paste a detail unlabelled text from anywhere; if you have no detail text  you have sample text in data/sample_texts.json
+    iii. if a prompt message ask 'paste' or 'paste as one line', must choose the last one
+    iv. press enter
+
+3. It may take some time for the first time depending on your cpu/gpu performane
--- train.py
+++ train.py
@@ -5,17 +5,22 @@ from config import CONFIG
 
 def main():
     # Load data
-    data = load_data("data/sample_texts.json")
-    
+    # data = load_data("data/sample_texts.json")
+    while True:
+        data=input("Input your text detail that expect to summarize: ")
+        if data !="":
+            break
+
     # Load model
     model = get_summarization_model(CONFIG["model_name"], CONFIG["device"])
     
     # Perform summarization
-    for item in data:
-        text = item["content"]
-        summary = summarize_text(model, text, CONFIG["max_input_length"], CONFIG["max_output_length"])
-        print(f"Original Text: {text}")
-        print(f"Summary: {summary}\n")
+    # for item in data:
+    # text = item["content"]
+    text = data
+    summary = summarize_text(model, text, CONFIG["max_input_length"], CONFIG["max_output_length"])
+    # print(f"Original Text: {text}")
+    print(f"Summary: {summary}\n")
 
 if __name__ == "__main__":
     main()
